e45d3eb57947a25249dd466d126bf1bf44685cbc
jintao <jintao@berachain.com>
2025-03-11 17:30:21 -0500

    chore: cleanup generation

--- sdk/boyco/stats/process.ts
+++ sdk/boyco/stats/process.ts
@@ -2,59 +2,9 @@ import positionsByMarket from './data/positions-by-market.json';
 import otherMarkets from './markets.json';
 import positionsByMarketByAccount from './data/positions-by-market-by-account.json';
 import { writeFileSync } from 'node:fs';
-import { overrideMarketMap } from './compare';
-// import { Contract, ethers } from 'ethers';
-// import { hexZeroPad } from 'ethers/lib/utils';
-
-// const provider = new ethers.providers.JsonRpcProvider("https://rpc.berachain.com");
-// const filter = {
-//     address: "0xe62987D430030C0A65BcE3C3cEE0F6F282ee4908",
-//     topics: [
-//         ethers.utils.id("RewardClaimed(bytes32,address,address,uint256)"),
-//     ]
-// };
-
-// const abi = [
-//     "event RewardClaimed(bytes32 indexed identifier, address indexed token, address indexed account, uint256 amount)"
-//   ];
-  
-// const contract = new Contract("0xe62987D430030C0A65BcE3C3cEE0F6F282ee4908", abi, provider);
-// const result = await contract.queryFilter(filter);
-// console.log(result);
+import { overrideMarketMap } from './market-map';
 
 const beraPrice = 6.75;
-
-const claims = Object.fromEntries(Object.entries({
-    ["0x9999b99AD237BaB0Dc8fd7aaf2CAceB7A8A89999"]: 4.943677709305667633,
-    "0xD248d2f09bFbe04e67fC7Fea08828D6AD6d95B6D": 8.39970015739336706,
-    "0xB168Eb2D0cBa59D1F59fB6f74520190Ec21C192f": 0.829995600780398356,
-    "0x4e937183B50f506e2D954948c588722dA0B2032E": 66.656538932200064096,
-    "0x546dcBD40E6FB1950a63FEF7C5c6b3eC296d7DBD": 0.042676758552912467,
-    "0x6B1769815Ab6556959999272CDe36C6062F3A3d8": 0.032850151385852843,
-    "0x14975679e5f87c25fa2c54958e735a79B5B93043": 195.748763971717977488, 
-    "0x154bEAFe33c12230f2b4cc758ec0eC41a65bA7Dc": 0.151561090715948088,
-    "0xeD594251d28AfeE844955D774e4FeE186F012D51": 1.010911566907401359,
-    "0xafCCb21e51B27B09272A0bf4D204623E706CDB39": 7.684775760950116385,
-    "0xB7249313d22590f7d336Fb256cae48eB96bb1547": 0.195311881154913269,
-    "0x2DA892095B107381C2D68B70Cd6EF045bC16815d": 1.753921607388037262,
-    "0x406149cb165Db1a0c1C8AA8d9310C4F36a7d6864": 0.829205908157412463,
-    "0x056906BDB6A508bf4209B48D2542aE6952a5f5AA": 0.235285828767334254,
-    "0x9a1654a15ccd68D04b353C7EA8d5791A9316c2F6": 0.162389866630350571,
-    "0x1E55Aae913d4De20A1Ec5e07D83C87dB6b0349c7": 2.015770347759462177, 
-    "0x917Be8168E62621eD9607283561FA1f7FD4e81e6": 1.677936764438205719,
-    "0xAC82362174A32fBBAd61Efcb0f02b9016B20C768": 0.353736616611616427,
-    "0xB4F17C28B4FeE29231c37Aa4B4Fc4b42a02D6673": 0.150372841292881892,
-    "0x3CE316D3B3d9A20F3e48e9da4d95892773e29F8e": 0.80640073902898246,
-    "0x22b908c2FeA7a1e6043FfcDBc77D660D4D326961": 2.52026947202091911,
-    "0xcc5B087283D69dB4237Da489d487140f2745fD28": 1.209601108543473913,
-    "0x67beb3Dd509b88b706dC5A9f03f50006410b088B": 0.431375773047898203, 
-    "0xED3981C6220dFcd368268b180b87AB643ce04b75": 239.86558804423955848,
-}).map((e) => [e[0].toLowerCase(), e[1]]));
-
-// console.log(`Found ${Object.values(claims).length} claims`);
-// const totalClaimed = Object.values(claims).reduce((t,c) => t += c, 0);
-// console.log(`Claimed ${totalClaimed} BERA`);
-// console.log(43988.291381501009248025 + totalClaimed);
 
 interface AccountEntitlement {
     [address: string]: {
@@ -97,7 +47,6 @@ function processData() {
         const usdValue = positionsByMarket[key].inputTokenAmountUSD;
         const marketData = marketLookup[marketId];
         if (!marketData) {
-            // console.log('No market data for', marketId);
             continue;
         }
         let duration = marketData.lockup_time ? Number(marketData.lockup_time) / (60 * 60 * 24) : 90;
@@ -107,7 +56,6 @@ function processData() {
         let multiplier = overrideMarketMap.find((e) => e.id === marketId)?.multiplier;
         if (!multiplier) {
             multiplier = 1;
-            // console.log('found missing multiplier for ', marketId);
         }
         const points = multiplier * usdValue;
         
@@ -124,14 +72,6 @@ function processData() {
             bucketOnePoints += points;
         }
 
-        // console.log({
-        //     marketId,
-        //     usdValue,
-        //     multiplier: marketData.multiplier,
-        //     duration,
-        //     points,
-        //     bucket
-        // });
         marketAllocations[marketId] = {
             name: marketData.name,
             marketId,
@@ -163,17 +103,6 @@ function processData() {
         entry.weight = pointsPercentage * 100;
         entry.apr = (((365 / entry.duration) * beraAllocation * beraPrice) / entry.usdValue) * 100;
     }
-
-    // console.log({
-    //     totalTvl: totalTvl.toLocaleString(),
-    //     totalPoints: totalPoints.toLocaleString(),
-    //     bucketOneTvl: bucketOneTvl.toLocaleString(),
-    //     bucketOnePoints: bucketOnePoints.toLocaleString(),
-    //     bucketTwoTvl: bucketTwoTvl.toLocaleString(),
-    //     bucketTwoPoints: bucketTwoPoints.toLocaleString(),
-    //     marketAllocations,
-    //     emitted: Object.values(marketAllocations).reduce((t, m) => t + m.amount, 0),
-    // });
 
     writeFileSync('top-level-data.json', JSON.stringify({
         totalTvl: totalTvl.toLocaleString(),
@@ -258,47 +187,12 @@ function processData() {
         accountEntitlements[entry.accountAddress][marketId] = entitlement;
     }
 
-    let removedAccounts = 0;
-    for (const [key, value] of Object.entries(thirtyDayAccountEntitlements)) {
-        const amount = Object.values(value).reduce((t, c) => t += c, 0);
-        const claimed = claims[key];
-
-        if (claimed > amount) {
-            leakedBera += claimed - amount;
-            // console.log(`${key} leaked ${claimed - amount}`)
-            delete thirtyDayAccountEntitlements[key];
-            removedAccounts++
-        } else if (claimed > 0) {
-            const owed = amount - claimed;
-            deficientBera += owed;
-            const entitlement = Object.values(value).reduce((t, c) => t + c, 0);
-            // console.log({
-            //     key,
-            //     amount,
-            //     claimed,
-            // })
-            const reduction = owed / entitlement;
-            // console.log(`${key} deficient ${owed}, reduce by ${((1 - reduction) * 100).toFixed(2)}%`);
-            Object.entries(value).forEach((e) => thirtyDayAccountEntitlements[key][e[0]] *= reduction);
-        }
-    }
-
-    // const testCheckEntitlements = Object.values(thirtyDayAccountEntitlements["0x14975679e5f87c25fa2c54958e735a79B5B93043".toLowerCase()]).reduce((t, c) => t += c, 0);
-
-    // console.log({
-    //     leakedBera,
-    //     deficientBera,
-    //     removedAccounts,
-    //     testCheckEntitlements
-    // });
-
     const totalTokensDistributed = Object.values(thirtyDayAccountEntitlements).reduce((t, c) => {
         const v = Object.values(c).reduce((t2, c2) => t2 += c2, 0);
         return t + v;
     }, 0)
 
     for (const [address, markets] of Object.entries(accountEntitlements)) {
-
         for (const [marketId, amount] of Object.entries(markets)) {
             const position = accountPositions[address].find((p) => marketId === p.marketId.slice(4));
             const duration = Number(position.lockup_time) / (60 * 60 * 24);
@@ -309,6 +203,15 @@ function processData() {
             }
         }
     }
+
+    // const csv = 'receiverAddress,value\n';
+    // for (let i = 0; i < Object.keys(thirtyDayAccountEntitlements).length; i+= 24) {
+    //     const addresses = Object.keys(thirtyDayAccountEntitlements).slice(i, i + 24);
+    //     const index = Math.floor(i / 24);
+    //     let csvContent = csv;
+    //     addresses.forEach((a) => csvContent = csvContent.concat(`${a},0.001\n`));
+    //     writeFileSync(`thirty-day-distro-${index}.csv`, csvContent);
+    // }
 
     writeFileSync('user-allocations.json', JSON.stringify(accountEntitlements, undefined, 2));
     writeFileSync('user-allocations-ninety-day.json', JSON.stringify(ninetyDayAccountEntitlements, undefined, 2));
@@ -327,6 +230,8 @@ function processData() {
     console.log(`Total Claimants: ${Object.keys(ninetyDayAccountEntitlements).length}`);
     console.log(`Distributed ${((tokensForNinetyDay / 10_000_000) * 100).toFixed(3)}% of Boyco`);
     console.log(`Distributed ${tokensForNinetyDay.toLocaleString()} BERA`);
+
+    console.log(`TotalDistribution: ${(tokensForThirtyDay + tokensForNinetyDay).toLocaleString()}`)
 }
 
 processData()
